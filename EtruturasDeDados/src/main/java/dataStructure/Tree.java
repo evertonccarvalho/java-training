4557d92ffa5968e4fad5498c00132b9c3b63a051
Éverton Carvalho <evertonsnake@gmail.com>
2024-06-11 10:15:07 -0300

    travessia

--- EtruturasDeDados/src/main/java/dataStructure/Tree.java
+++ EtruturasDeDados/src/main/java/dataStructure/Tree.java
@@ -46,20 +46,74 @@ public class Tree {
         }
     }
 
+    public void preOrder(){
+        preOrder(root);
+    }
+
+    private void preOrder(final Node node) {
+        // R E D
+        if(node == null) return;
+        System.out.println(node.value);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void inOrder(){
+        inOrder(root);
+    }
+
+    private void inOrder(final Node node) {
+        // R E D
+        if(node == null) return;
+        inOrder(node.left);
+        System.out.println(node.value);
+        inOrder(node.right);
+    }
+
+    public void posOrder(){
+        posOrder(root);
+    }
+
+    private void posOrder(final Node node) {
+        // R E D
+        if(node == null) return;
+        posOrder(node.left);
+        posOrder(node.right);
+        System.out.println(node.value);
+    }
+
+
     public static void main(String[] args) {
         Tree tree = new Tree();
-        tree.insert(1);
-        tree.insert(2);
-        tree.insert(3);
+        tree.insert(37);
+        tree.insert(11);
+        tree.insert(66);
+        tree.insert(8);
+        tree.insert(17);
+        tree.insert(42);
+        tree.insert(72);
 
-        System.out.println(tree.root.value);
-        System.out.println(tree.root.left.value);
-        System.out.println(tree.root.right.value);
-
-        System.out.println(tree.root.isLeaf());
-        System.out.println(tree.root.right.isLeaf());
-
+        System.out.println("####################");
+//        tree.preOrder();
+//        tree.inOrder();
+        tree.posOrder();
+        System.out.println("####################");
     }
+
+//    public static void main(String[] args) {
+//        Tree tree = new Tree();
+//        tree.insert(1);
+//        tree.insert(2);
+//        tree.insert(3);
+//
+//        System.out.println(tree.root.value);
+//        System.out.println(tree.root.left.value);
+//        System.out.println(tree.root.right.value);
+//
+//        System.out.println(tree.root.isLeaf());
+//        System.out.println(tree.root.right.isLeaf());
+//
+//    }
 
 
 }
